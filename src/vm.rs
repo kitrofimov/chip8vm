8ff14fcffdf7b911ca5c680b28b24a5ed14b5910
Kirill Trofimov <kitrofimov39@gmail.com>
2025-04-03 21:55:22 +0500

    Cosmetic changes

--- src/vm.rs
+++ src/vm.rs
@@ -242,6 +242,10 @@ impl<'a> VM<'a> {
         self.canvas.present();
     }
 
+    // 1 2 3 C        1 2 3 4
+    // 4 5 6 D  <==>  Q W E R
+    // 7 8 9 E  <==>  A S D F
+    // A 0 B F        Z X C V
     fn scancode_to_chip8_key(scancode: Scancode) -> Option<u8> {
         match scancode {
             Scancode::Num1 => Some(0x1),
