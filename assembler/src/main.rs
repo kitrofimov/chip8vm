3e258b94e8ee6657afa25783449df84ea4eceb3a
Kirill Trofimov <kitrofimov39@gmail.com>
2025-04-25 20:56:00 +0500

    Add user-friendly error handling to `main`

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -15,6 +15,15 @@ fn main() {
     let input_path = &args[1];
     let output_path = &args[2];
 
-    let bytecode = assembler::assemble_from_file(&input_path).unwrap();
-    fs::write(output_path, bytecode).expect("Failed to write output file");
+    let bytecode = assembler::assemble_from_file(&input_path)
+        .unwrap_or_else(|e| {
+            eprintln!("Failed to assemble! {}", e);
+            std::process::exit(1);
+        });
+
+    fs::write(output_path, bytecode)
+        .unwrap_or_else(|_| {
+            eprintln!("Failed to write to output file: {}", output_path);
+            std::process::exit(2);
+        });
 }
