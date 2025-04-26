e3eaf2bbd1479c92f09560c1706f5f4d1013ffbc
Kirill <kitrofimov39@gmail.com>
2025-04-26 17:00:50 +0500

    Modify sample assembler code to test some assembler directives

--- roms/simple_drawing.asm
+++ roms/simple_drawing.asm
@@ -8,8 +8,15 @@ DRW V0, V1, 15
 loop:
 JP loop
 
+.space 5
+.fill 3, 0xFF
+.space 5
+; .text "Hello, world!"
+; .space 5
+
 sprite:
-.word 0b1111000000001111
+.byte 0b11110000
+.byte 0b00001111
 .word 0b1111000000001111
 .word 0b1111000000001111
 .word 0b1111000000001111
