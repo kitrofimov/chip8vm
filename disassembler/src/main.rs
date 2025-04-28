df7293176ffdc26c1364f2b0a59acfc178d4d912
Kirill <kitrofimov39@gmail.com>
2025-04-28 20:32:59 +0500

    Implement basic program flow

--- disassembler/src/main.rs
+++ disassembler/src/main.rs
@@ -1,3 +1,35 @@
+use std::{env, fs};
+use colored::Colorize;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        eprintln!("Usage: {} <input.ch8> <output.asm>", args[0]);
+        return;
+    }
+
+    let input_path = &args[1];
+    let output_path = &args[2];
+
+    let bytecode = fs::read(input_path)
+        .unwrap_or_else(|e| {
+            error(format!("failed to read input file: {}", e.to_string()));
+            std::process::exit(1);
+        });
+    let asm = disassemble(bytecode);
+
+    fs::write(output_path, asm)
+        .unwrap_or_else(|e| {
+            error(format!("failed to write to output file: {}", e.to_string()));
+            std::process::exit(2);
+        });
+}
+
+fn disassemble(bytecode: Vec<u8>) -> String {
+    unimplemented!();
+}
+
+fn error(error_message: String) {
+    let error_title = "error:".red().bold();
+    eprintln!("{} {}", error_title, error_message);
 }
